887af46e2956651b8bb700b37fc81dc777fa3e88
she13lest <she13lest@gmail.com>
2021-11-25 16:57:18 +0200

    commit

---
+++ lesson13js/index.test.js
@@ -0,0 +1,16 @@
+it('17 means 17', () => {
+  expect(17).toEqual(17);
+});
+
+it('18 not equal 17', () => {
+  expect(18).not.toEqual(17);
+});
+
+const getEvenNumbers = (numbers) => numbers.filter((num) => num % 2 == 0);
+
+getEvenNumbers([1, 2, 3, 4]);
+
+it('get even numbers from arr', () => {
+    const result = getEvenNumbers([1, 2, 3, 4]);
+    expect(result).toEqual([2, 4])
+});
