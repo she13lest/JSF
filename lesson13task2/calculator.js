4a25dc2c32f491139d7e0197e8af1e81d34b6be8
she13lest <she13lest@gmail.com>
2021-11-28 18:13:28 +0200

    commit

---
+++ lesson13task2/calculator.js
@@ -0,0 +1,5 @@
+export const getSquaredArray = numbers => numbers.map(num => num * num);
+
+export const getOddNumbers = numbers => numbers.filter(num => num % 2 === 1);
+
+export default (a, b) => a + b;
