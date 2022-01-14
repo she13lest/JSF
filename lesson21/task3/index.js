893cbcea3ffbb4b9514bf22efff6fdb078c00f7c
she13lest <she13lest@gmail.com>
2022-01-14 13:34:28 +0200

    new

--- lesson21/task3/index.js
+++ lesson21/task3/index.js
@@ -1,10 +1,10 @@
-export function getItemList() {
+export function getItemsList() {
   const elementsList = document.querySelectorAll('.technology');
   console.dir(elementsList);
   return elementsList;
 }
 
-export function getItemArray() {
+export function getItemsArray() {
   const elementsArray = Array.from(document.querySelectorAll('.tool'));
   console.dir(elementsArray);
   return elementsArray;
