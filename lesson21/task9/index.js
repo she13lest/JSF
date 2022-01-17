4a07d024c0948389e4de949befaac8f81a5e07fe
she13lest <she13lest@gmail.com>
2022-01-17 19:59:15 +0200

    new

--- lesson21/task9/index.js
+++ lesson21/task9/index.js
@@ -1,6 +1,6 @@
 export function finishList() {
   const listOfEl = document.querySelector('.list');
-  const spesialEl = document.querySelector('.special');
+  const specialEl = document.querySelector('.special');
 
   function createdEl(num) {
     const elOfList = document.createElement('li');
@@ -10,7 +10,6 @@ export function finishList() {
 
   listOfEl.append(createdEl(8));
   listOfEl.prepend(createdEl(1));
-  spesialEl.before(createdEl(4));
-  spesialEl.after(createdEl(6));
+  specialEl.before(createdEl(4));
+  specialEl.after(createdEl(6));
 }
-finishList();
