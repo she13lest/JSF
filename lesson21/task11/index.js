08477403e32928997457ccfc3a2bd24c34624dbc
she13lest <she13lest@gmail.com>
2022-01-20 00:16:25 +0200

    new

--- lesson21/task11/index.js
+++ lesson21/task11/index.js
@@ -1,12 +1,12 @@
 export function manageClasses() {
   const first = document.querySelector('.one');
-  first.classList.add('.selected');
+  first.classList.add('selected');
   const second = document.querySelector('.two');
-  second.classList.remove('.selected');
+  second.classList.remove('selected');
   const thirt = document.querySelector('.three');
-  thirt.classList.toggle('.three_done');
+  thirt.classList.toggle('three_done');
   const fourth = document.querySelector('.four');
   if (fourth.classList.containes('.some-class')) {
-    fourth.classList.add('.another-class');
+    fourth.classList.add('another-class');
   }
 }
