17a376e545c345285afee098ba68b6a5757cdd51
she13lest <she13lest@gmail.com>
2022-01-20 00:20:42 +0200

    new

--- lesson21/task11/index.js
+++ lesson21/task11/index.js
@@ -1,12 +1,4 @@
 export function manageClasses() {
-  const first = document.querySelector('.one');
-  first.classList.add('selected');
-  const second = document.querySelector('.two');
-  second.classList.remove('selected');
-  const thirt = document.querySelector('.three');
-  thirt.classList.toggle('three_done');
-  const fourth = document.querySelector('.four');
-  if (fourth.classList.containes('.some-class')) {
-    fourth.classList.add('another-class');
-  }
+  const one = document.querySelector('.one');
+  one.classList.add('selected');
 }
