7b637568bfb2e88950f1417e1e60d4667c222719
she13lest <she13lest@gmail.com>
2022-01-22 19:58:00 +0200

    new

--- lesson21/task13/index.js
+++ lesson21/task13/index.js
@@ -1,7 +1,5 @@
 export function getSection(num) {
   const spanWithNum = document.querySelector(`span[data-number="${num}"]`);
-  const parentEl = spanWithNum.closest('box');
+  const parentEl = spanWithNum.parentNode;
   return parentEl.getAttribute('data-section');
 }
-
-console.log(getSection(3));
