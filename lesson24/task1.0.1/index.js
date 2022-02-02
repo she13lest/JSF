5850f9ee89bc9bd821cdbe82457c6e43fb13a15b
she13lest <she13lest@gmail.com>
2022-02-02 14:04:04 +0200

    new

---
+++ lesson24/task1.0.1/index.js
@@ -0,0 +1,11 @@
+export function getGreenwichTime(date) {
+  const formatter = new Intl.DateTimeFormat('en', {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  });
+
+  return formatter.format(date);
+}
+
+console.log(getGreenwichTime(new Date()));
