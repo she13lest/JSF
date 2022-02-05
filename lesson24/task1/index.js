c2f64af58d891f485aab6fc79815bbc7a459fdf0
she13lest <she13lest@gmail.com>
2022-02-05 10:45:39 +0200

    new

--- lesson24/task1/index.js
+++ lesson24/task1/index.js
@@ -1,13 +1,17 @@
 export function getDiff(startDate, endDate) {
   const diff = Math.abs(endDate.getTime() - startDate.getTime());
-  const days = Math.ceil(diff / (1000 * 3600 * 24));
-  const hours = Math.ceil((diff / (1000 * 60 * 60)) % 24);
-  const minutes = Math.ceil((diff / (1000 * 60)) % 60);
-  const seconds = Math.ceil((diff / 1000) % 60);
+  const days = Math.trunc(diff / (1000 * 3600 * 24));
+  const hours = Math.trunc((diff / (1000 * 60 * 60)) % 24);
+  const minutes = Math.trunc((diff / (1000 * 60)) % 60);
+  const seconds = Math.trunc((diff / 1000) % 60);
   return `${days}d ${hours}h ${minutes}m ${seconds}s`;
 }
 
 const startDate = new Date(2014, 10, 2, 4, 5);
 const endDate = new Date(2021, 0, 3, 2, 1);
 
-getDiff(startDate, endDate);
+const case1 = getDiff(startDate, endDate);
+console.log(case1)
+
+const case2 = getDiff(new Date(2021, 10, 1, 4, 1), new Date(2020, 0, 2))
+console.log(case2);
