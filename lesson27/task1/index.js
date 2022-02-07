b93c28deba24ed3fad12dda07fec5ce09f63db93
she13lest <she13lest@gmail.com>
2022-02-07 20:13:16 +0200

    new

--- lesson27/task1/index.js
+++ lesson27/task1/index.js
@@ -1,12 +1,20 @@
 localStorage.clear();
+
 localStorage.setItem('usher1', JSON.stringify({ name: 'Bob' }));
 localStorage.setItem('name', JSON.stringify('Violine'));
 localStorage.setItem('number', JSON.stringify(23));
+localStorage.setItem('again', JSON.stringify(true));
 
 // console.log(JSON.parse(localStorage.getItem('number')));
 
-function getLocalStorageDate() {
+export function getLocalStorageDate() {
   return Object.entries(localStorage).reduce((acc, [key, value]) => {
+    let newValue;
+    try {
+      newValue = JSON.parse(value);
+    } catch (e) {
+      newValue = value;
+    }
     return {
       ...acc,
       [key]: JSON.parse(value),
