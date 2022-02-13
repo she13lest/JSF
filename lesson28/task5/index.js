40d19e6309838fba55ea4f9baba0bd229d429469
she13lest <she13lest@gmail.com>
2022-02-13 20:28:08 +0200

    commit

---
+++ lesson28/task5/index.js
@@ -0,0 +1,49 @@
+function shmoment(date) {
+    const result = new Date(date);
+
+    const changingDate = {
+
+    add(type, value) {
+        if (type === 'years') {
+            result.setFullYear(result.getFullYear() + value);
+        } else if (type === 'months') {
+            result.setMonth(result.getMonth() + value);
+        } else if (type === 'days') {
+            result.setDate(result.getDate() + value);
+        } else if (type === 'hours') {
+          result.setHours(result.getHours() + value);
+        } else if (type === 'minutes') {
+          result.setMinutes(result.getMinutes() + value);
+        } else if (type === 'seconds') {
+          result.setSeconds(result.getSeconds() + value);
+        } else if (type === 'milliseconds') {
+          result.setMilliseconds(result.getMilliseconds() + value);
+        } return this;
+    },
+    subtract(type, value) {
+        if (type === 'years') {
+            result.setFullYear(result.getFullYear() + value);
+        } else if (type === 'months') {
+            result.setMonth(result.getMonth() + value);
+        } else if (type === 'days') {
+            result.setDate(result.getDate() + value);
+        } else if (type === 'hours') {
+          result.setHours(result.getHours() + value);
+        } else if (type === 'minutes') {
+          result.setMinutes(result.getMinutes() + value);
+        } else if (type === 'seconds') {
+          result.setSeconds(result.getSeconds() + value);
+        } else if (type === 'milliseconds') {
+          result.setMilliseconds(result.getMilliseconds() + value);
+        } return this;
+        }, 
+        result() {
+            return result;
+        }
+    }
+    return changingDate;
+}
+
+const newCount = shmoment(new Date(2020, 0, 7, 17, 17, 17)).add('minutes', 2).add('days', 8).subtract('years', 1).result();
+
+console.log(newCount)
