d118a769fbb8a14ff3b0f67f5814f9371fcd098e
she13lest <she13lest@gmail.com>
2022-02-18 19:19:42 +0200

    commit

---
+++ lesson31/task1/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @param {string} userId
+ * @return {promise}
+ */
+export const requestUserData = userId => {
+  const request = new Promise((resolve, reject) => {
+    if (userId === 'broken') {
+      setTimeout(() => {
+        reject(new Error('User not found'));
+      }, 500);
+    } else {
+      setTimeout(() => {
+        resolve({
+          name: 'John',
+          age: 17,
+          userId: `${userId}`,
+          email: userId,
+        });
+      }, 1000);
+    }
+  });
+
+  return request;
+};
+
+requestUserData('broken')
+  // .then(data => console.log(data));
+  // .catch (error => console.log(error));
+  .finally(() => console.log('finally'));
