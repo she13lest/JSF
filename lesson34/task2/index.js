1ec25992f9edfd4a1934263f4a8538cc5c84b467
Anna <she13lest@gmail.com>
2022-06-14 13:22:47 +0200

    new

--- lesson34/task2/index.js
+++ lesson34/task2/index.js
@@ -34,37 +34,37 @@ export function updateUser(userId, userData) {
   });
 }
 
-// // examples
-// getUsersList().then((users) => {
-//   console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
-// });
+// examples
+getUsersList().then((users) => {
+  console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
+});
 
-// getUserById("3").then((userData) => {
-//   console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
-// });
+getUserById("3").then((userData) => {
+  console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
+});
 
-// const newUserData = {
-//   email: "cool@email.com",
-//   firstName: "Iron",
-//   lastName: "Man",
-//   age: 42,
-// };
+const newUserData = {
+  email: "cool@email.com",
+  firstName: "Iron",
+  lastName: "Man",
+  age: 42,
+};
 
-// createUser(newUserData).then(() => {
-//   console.log("User created");
-// });
+createUser(newUserData).then(() => {
+  console.log("User created");
+});
 
-// const updatedUserData = {
-//   email: "new@email.com",
-//   firstName: "John",
-//   lastName: "Doe",
-//   age: 17,
-// };
+const updatedUserData = {
+  email: "new@email.com",
+  firstName: "John",
+  lastName: "Doe",
+  age: 17,
+};
 
-// updateUser("1", updatedUserData).then(() => {
-//   console.log("User updated");
-// });
+updateUser("1", updatedUserData).then(() => {
+  console.log("User updated");
+});
 
-// deleteUser("3").then(() => {
-//   console.log("User updated");
-// });
+deleteUser("3").then(() => {
+  console.log("User updated");
+});
